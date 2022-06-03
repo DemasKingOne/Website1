2da861a8e9f419bb6ab43918ae36596dfe2c9746
DemasKingOne <korolkov2002dima@mail.ru>
2022-06-03 18:26:59 +0300

    lesson 4

--- Project/Script.js
+++ Project/Script.js
@@ -1,32 +1,52 @@
 function guess () {
 
-    var guessNumber = 58010575;
+    var guessNumber = 5;
 
     var ourNumber = prompt ("Введите число:");
 
     if (ourNumber > guessNumber) {
-        alert ("Число должно быть меньше");
+        alert ("Число должно быть меньше!");
         return guess ();
     } 
+
     else if (ourNumber == guessNumber) {
-        return alert ("Вы угадали");
+        return alert ("Вы угадали!");
     } 
+ 
     else {
-        alert ("Число должно быть больше");
+        alert ("Число должно быть больше!");
         return guess ();
     }
+
 }
 
 function reminder () {
     alert ("Ты  здесь слишком долго")
+    window.close ();
 }
 
 //setTimeout (reminder, 3000);
 
-function valideForm () {
-    alert ("Проверка формы");
-    var name = document.qetElementById ("name").value;
-    console.log (name);
+function valideForm() {
+    alert ("Проверка формы!");
+    
+    var name = document.getElementById("name").value;
+    var password = document.getElementById("password").value;
+    
+    //console.log (name);
+    //console.log (password);
+    
+    var reg_name = /^[а-яa-zё]+$/i;
+    var reg_password = /^[0-9a-z]{5,100}/i;
+    
+    if (reg_name.test(name) == false) {
+        alert ("Ошибка в поле ФИО!");
+    }
+    
+    if (reg_password.test(password) == false) {
+        alert ("Ошибка в поле пароль!");
+    }
+    
 }
 
-document.querySelector (".button").addEventListener ("click", valideForm);
+document.querySelector(".button").addEventListener("click", valideForm);
